a0a907ed431b15a8c3862b6f5f72d8d8b219e1e6
dan <dan@wat.(none)>
2014-05-07 21:00:04 +0300

    add new GeoLocation model

---
+++ trolly/faver/migrations/0006_auto__add_geolocation__del_field_station_long__del_field_station_lat__.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'GeoLocation'
+        db.create_table(u'faver_geolocation', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('lat', self.gf('django.db.models.fields.CharField')(max_length=10, null=True, blank=True)),
+            ('long', self.gf('django.db.models.fields.CharField')(max_length=10, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'faver', ['GeoLocation'])
+
+        # Deleting field 'Station.long'
+        db.delete_column(u'faver_station', 'long')
+
+        # Deleting field 'Station.lat'
+        db.delete_column(u'faver_station', 'lat')
+
+        # Adding field 'StationStop.location'
+        db.add_column(u'faver_stationstop', 'location',
+                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['faver.GeoLocation']),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting model 'GeoLocation'
+        db.delete_table(u'faver_geolocation')
+
+        # Adding field 'Station.long'
+        db.add_column(u'faver_station', 'long',
+                      self.gf('django.db.models.fields.CharField')(max_length=10, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Station.lat'
+        db.add_column(u'faver_station', 'lat',
+                      self.gf('django.db.models.fields.CharField')(max_length=10, null=True, blank=True),
+                      keep_default=False)
+
+        # Deleting field 'StationStop.location'
+        db.delete_column(u'faver_stationstop', 'location_id')
+
+
+    models = {
+        u'faver.geolocation': {
+            'Meta': {'object_name': 'GeoLocation'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lat': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'long': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'})
+        },
+        u'faver.route': {
+            'Meta': {'object_name': 'Route'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nr': ('django.db.models.fields.IntegerField', [], {}),
+            'stops': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['faver.StationStop']", 'symmetrical': 'False'})
+        },
+        u'faver.station': {
+            'Meta': {'object_name': 'Station'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'rtec_id': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'faver.stationstop': {
+            'Meta': {'object_name': 'StationStop'},
+            'day_type': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['faver.GeoLocation']"}),
+            'order_nr': ('django.db.models.fields.IntegerField', [], {}),
+            'station': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['faver.Station']"})
+        },
+        u'faver.stoptime': {
+            'Meta': {'object_name': 'StopTime'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'station': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'stopstime'", 'null': 'True', 'to': u"orm['faver.StationStop']"}),
+            'time': ('django.db.models.fields.DateTimeField', [], {})
+        }
+    }
+
+    complete_apps = ['faver']
--- trolly/faver/models.py
+++ trolly/faver/models.py
@@ -6,12 +6,22 @@ class Station(models.Model):
     Model responsible for stations
     """
     name = models.CharField(max_length=255, verbose_name=u'Name')
-    lat = models.CharField(max_length=10, verbose_name=u'Latitude', blank=True, null=True)
-    long = models.CharField(max_length=10, verbose_name=u'Longitude', blank=True, null=True)
     rtec_id = models.IntegerField(u'RTEC ID', blank=True, null=True)
 
     def __unicode__(self):
         return self.name
+
+
+class GeoLocation(models.Model):
+    """
+    Model representing the geolocation
+    of a certain station
+    """
+    lat = models.CharField(max_length=10, verbose_name=u'Latitude', blank=True, null=True)
+    long = models.CharField(max_length=10, verbose_name=u'Longitude', blank=True, null=True)
+
+    def __unicode__(self):
+        return unicode("{0} - {1}".format(self.lat, self.long))
 
 
 class StationStop(models.Model):
@@ -20,11 +30,12 @@ class StationStop(models.Model):
     """
     WEEKDAY_CHOICES = ((1,'Mon-Fri'), (2, 'Sat'), (3, 'Sun'))
     station = models.ForeignKey(Station, verbose_name=u'Station')
+    location = models.ForeignKey(GeoLocation, verbose_name=u'Geo Location')
     order_nr = models.IntegerField(u'Order number')
     day_type = models.IntegerField(choices=WEEKDAY_CHOICES, verbose_name=u'Weekday Type')
 
     def __unicode__(self):
-        return unicode(self.station.name)
+        return unicode(self.station)
 
 
 class StopTime(models.Model):
@@ -35,7 +46,7 @@ class StopTime(models.Model):
     station = models.ForeignKey(StationStop, related_name='stopstime', blank=True, null=True)
 
     def __unicode__(self):
-        return "{0} - {1}".format(self.station.Station.name, self.time)
+        return "{0} - {1}".format(self.time, self.station)
 
 
 class Route(models.Model):
