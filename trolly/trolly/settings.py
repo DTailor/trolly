c321aa6339869c5daafaffd81a2b431451cf8985
DTailor <dann.croitoru@gmail.com>
2014-05-20 20:57:05 +0300

    modify static/media directory

--- trolly/trolly/settings.py
+++ trolly/trolly/settings.py
@@ -1,5 +1,12 @@
+from os.path import abspath, basename, dirname, join, normpath
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DJANGO_ROOT = dirname(dirname(abspath(__file__)))
+SITE_ROOT = dirname(DJANGO_ROOT)
+
+SITE_NAME = basename(DJANGO_ROOT)
 
 DEBUG = True
 
@@ -41,16 +48,26 @@ USE_I18N = True
 
 USE_L10N = True
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
+TEMPLATE_DIRS = (
+    normpath(join(SITE_ROOT, 'trolly', 'templates')),
+)
 
-
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'faver', 'media'))
+MEDIA_ROOT = normpath(join(SITE_ROOT, 'trolly', 'media'))
 
 MEDIA_URL = '/media/'
-
-STATIC_ROOT = (os.path.join(BASE_DIR, 'faver', 'static'))
+STATIC_ROOT = normpath(join(SITE_ROOT, 'trolly', 'assets'))
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    normpath(join(SITE_ROOT, 'trolly', 'static')),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 
 try:
