d209ec452fdb3ba685b3c607dfaaaac77fb971d4
DTailor <dann.croitoru@gmail.com>
2014-05-22 00:11:09 +0300

    add to admin waypoint

--- trolly/faver/admin.py
+++ trolly/faver/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Station, Route, StationStop, StopTime, GeoLocation
+from .models import Station, Route, StationStop, StopTime, \
+                    GeoLocation, WayPoint
 
 class StationAdmin(admin.ModelAdmin):
     pass
@@ -17,9 +18,13 @@ class StationStopAdmin(admin.ModelAdmin):
 class StopTimeAdmin(admin.ModelAdmin):
     pass
 
+class WayPointAdmin(admin.ModelAdmin):
+    list_display = ('route', 'lat', 'long')
+
 
 admin.site.register(Station, StationAdmin)
 admin.site.register(Route, RouteAdmin)
 admin.site.register(StationStop, StationStopAdmin)
 admin.site.register(StopTime, StopTimeAdmin)
 admin.site.register(GeoLocation, GeoLocationAdmin)
+admin.site.register(WayPoint, WayPointAdmin)
